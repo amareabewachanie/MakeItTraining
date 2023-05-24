733f1a55490ac42dcd06d79577ae3ca1798af865
Amare Abewa Chanie <amare.abewa@gmail.com>
2023-05-24 19:12:25 +0300

    Add project files.

---
+++ EmployeeManagmentMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeManagmentMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
